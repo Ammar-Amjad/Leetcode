29c6483255ec3907ecd56a384a86a993c1285b47
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-12 11:06:00 -0400

    Time: 146 ms, Memory: 19.1 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,14 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        Sum = 0
         dic = {}
         
         for n in nums:
             if n in dic:
-                del dic[n]
+                dic[n] += 1
+                res += (2 * n)
             else:
-                dic[n] = 1 
-        for k, v in dic.items():
-            if v == 1:
-                return k 
+                dic[n] = 1
+            Sum += n
         
-        
+        return Sum - res
