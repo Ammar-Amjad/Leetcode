d34e6bb53b6398037b8db7ec91331d318711f8c5
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-14 15:27:38 -0400

    Time: 119 ms, Memory: 19.6 MB - LeetHub

---
+++ moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -0,0 +1,20 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.queue = [0 for _ in range(size)]
+        self.head = 0
+        self.tail = 0
+        self.curlen = 0
+        self.size = size
+
+    def next(self, val: int) -> float:
+        self.queue[self.tail % self.size] = val
+        self.tail += 1
+        if self.curlen != self.size:
+            self.curlen += 1
+        return sum(self.queue) / self.curlen if self.curlen != 0 else 0
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
