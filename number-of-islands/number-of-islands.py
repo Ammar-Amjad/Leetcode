27e7d9ec2d5a025db9a7d3eb08df1ba89d5dbd07
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-15 20:57:00 -0400

    Time: 322 ms, Memory: 19.1 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,23 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        queue = deque()
-        islands = 0
-        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        rLen = len(grid)
-        cLen = len(grid[0])
-
-        for i in range(rLen):
-            for j in range(cLen):
-                if grid[i][j] == '1':
-                    islands += 1
-                    queue.append((i, j))
-                    grid[i][j] = '0'
-                while queue:
-                    r, c = queue.popleft()
-                    
-                    for x, y in directions:
-                        if 0 <= x + r < rLen and 0 <= y + c < cLen and grid[x + r][y +c] == '1':
-                            queue.append((x + r, y + c))
-                            grid[x + r][y + c] = '0'
         
-        return islands
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+                return 0
+            if grid[i][j] == '0':
+                return 0
+
+            grid[i][j] = '0'
+            
+            dfs(i, j + 1)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
+            dfs(i + 1, j)
+            
+            return 0 
+        
+        count = 0
+        
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == '1':
+                    count += 1
+                    dfs(x, y)
+                
+        return count
+        
