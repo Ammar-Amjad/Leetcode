16bd37f3e0ff5caee4e7c09fba0c05bb63d2d54c
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-16 13:06:12 -0400

    Time: 95 ms, Memory: 16.5 MB - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,23 +1,48 @@
 class Solution:
     def floodFill(self, grid: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        # BFS
-        stack = [(sr, sc)]
-        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        # DFS
         orig_color = grid[sr][sc]
+        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        visited = set()
         
-        if orig_color == color:
-            return grid
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+                return 0
+            if grid[i][j] == orig_color and (i, j) not in visited:
+                grid[i][j] = color
+                visited.add((i, j))
+                
+                for x, y in directions:
+                    dfs(i + x, j + y)            
+                
+            else:
+                return 0                            
         
-        while stack: 
-            sr, sc = stack.pop()
-            grid[sr][sc] = color
-            for i, j in directions:
-                sri = sr + i
-                scj = sc + j
-                if 0 <= sri < len(grid) and 0 <= scj < len(grid[0]) and (sri, scj) not in stack:
-                    if grid[sri][scj] == orig_color:
-                        stack.append((sri,scj))
+        dfs(sr, sc)
         return grid
         
-        # DFS
+        
+        
+        
+        
+#         # BFS
+#         stack = [(sr, sc)]
+#         directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+#         orig_color = grid[sr][sc]
+        
+#         if orig_color == color:
+#             return grid
+        
+#         while stack: 
+#             sr, sc = stack.pop()
+#             grid[sr][sc] = color
+#             for i, j in directions:
+#                 sri = sr + i
+#                 scj = sc + j
+#                 if 0 <= sri < len(grid) and 0 <= scj < len(grid[0]) and (sri, scj) not in stack:
+#                     if grid[sri][scj] == orig_color:
+#                         stack.append((sri,scj))
+#         return grid
+        
+        
         
