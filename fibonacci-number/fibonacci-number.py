f249e230aacfe7aca6fb345d9cc54bfb57a25cad
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-19 17:37:13 -0400

    Time: 45 ms, Memory: 16.4 MB - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,5 +1,15 @@
 class Solution:
+    def __init__(self):
+        self.cache = {}
+        
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        if n in self.cache:
+            return self.cache[n]
+        else:
+            result = self.fib(n - 1) + self.fib(n - 2)
+        
+        self.cache[n] = result
+        
+        return result
