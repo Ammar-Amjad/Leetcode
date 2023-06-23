ea3e9156c9be0de6aab9f804b36c9ee5ce5875e7
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-23 11:46:59 -0400

    Time: 56 ms, Memory: 16.7 MB - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,22 +1,43 @@
+def minRSA(nums):
+    L = 0
+    R = len(nums) - 1
+    N = len(nums)
+    
+    while L <= R:
+        M = (L + R) // 2
+        
+        if nums[M] <= nums[(M - 1 + N) % N] and nums[M] <= nums[(M + 1) % N]:
+            return M
+        elif nums[M] <= nums[R]:
+            R = (M - 1 + N) % N
+        elif nums[M] >= nums[L]:
+            L = (M + 1) % N
+        
+    return L
+
+def BS(nums, L, R, target):
+    while L <= R:
+        M = (L + R) // 2
+        if nums[M] == target:
+            return M
+        elif target < nums[M]:
+            R = M - 1
+        else:
+            L = M + 1
+    return -1
+    
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        L = 0
-        R = len(nums) - 1
+        minIdx = minRSA(nums)
+        print(minIdx)
+        LR = BS(nums, 0, minIdx - 1, target)
+        RR = BS(nums, minIdx, len(nums) - 1, target) 
         
-        while L <= R:
-            M = (L + R) // 2
-            
-            if nums[M] == target:
-                return M
-            elif nums[L] <= nums[M]:
-                if target < nums[L] or target > nums[M]:
-                    L = M + 1
-                else:
-                    R = M - 1
-                    
-            else:
-                if target < nums[M] or target > nums[R]:
-                    R = M - 1
-                else:
-                    L = M + 1
-        return -1
+        if LR != -1:
+            return LR
+        elif RR != -1:
+            return RR
+        else:
+            return -1
+        
+        
