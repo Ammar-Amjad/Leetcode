78b6e782dad7f2ec3ed99ea7cc2aa14fa0056b78
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-24 19:02:55 -0400

    Time: 49 ms, Memory: 18.5 MB - LeetHub

--- closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -1,25 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution: 
-        
-    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        self.res = float('inf')
-        def dfs(node):
-            if node is None:
-                return None
-            
-            Val = abs(node.val - target)
-            OVal = abs(self.res - target) 
-            if Val == OVal and node.val < self.res:
-                self.res = node.val
-            elif Val < OVal:
-                self.res = node.val
-            dfs(node.left)
-            dfs(node.right)
-            
-        dfs(root) 
-        return self.res
+class Solution:
+    def closestValue(self, root, target):
+        closest = root.val
+        while root:
+            if abs(target - root.val) < abs(target - closest) or (abs(target - root.val) == abs(target - closest) and root.val < closest):
+                closest = root.val
+            root = root.left if target < root.val else root.right
+        return closest
+
