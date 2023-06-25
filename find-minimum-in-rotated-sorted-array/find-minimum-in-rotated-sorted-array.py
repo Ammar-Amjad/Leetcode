4df884cfea5a1575f57d86859613fce90546ee5a
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-06-25 10:52:44 -0400

    Time: 57 ms, Memory: 16.7 MB - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -7,10 +7,11 @@ class Solution:
         while L <= R:
             M = (L + R) // 2
             
-            if nums[M] <= nums[(M - 1 + N) % N] and nums[M] <= nums[(M + 1) % N]:
+            if nums[(M - 1 + N) % N] >= nums[M] <= nums[(M + 1) % N]:
                 return nums[M]
             elif nums[M] <= nums[R]:
                 R = (M - 1 + N) % N
             elif nums[M] >= nums[L]:
                 L = (M + 1) % N
         return nums[L]
+         
