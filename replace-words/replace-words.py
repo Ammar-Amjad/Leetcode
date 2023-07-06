57532473b5a798d30290d02f7e5ebf9518c2851f
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-06 15:48:30 -0400

    Time: 159 ms, Memory: 38.3 MB - LeetHub

---
+++ replace-words/replace-words.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.eow = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        node = self.root
+        
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.eow = True        
+        
+    def startsWith(self, word):
+        node = self.root
+        res = ''
+        
+        for c in word:  
+            if c not in node.children: 
+                return word
+            node = node.children[c] 
+            if node.eow == True:
+                return res + c
+            else:
+                res += c
+        return res
+        
+        
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        
+        for d in dictionary:
+            trie.insert(d)
+        
+        result = []
+        for s in sentence.split(' '):
+            result.append(trie.startsWith(s))
+        return ' '.join(result)
+            
+            
