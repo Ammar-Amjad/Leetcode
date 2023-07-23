324d5e7211f8bb6b9e58b169384426d609354256
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-23 14:21:21 -0400

    Time: 112 ms, Memory: 18.4 MB - LeetHub

--- delete-and-earn/delete-and-earn.py
+++ delete-and-earn/delete-and-earn.py
@@ -1,15 +1,20 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         dic = defaultdict(int)
+        max_num = -float('inf')
+        
+        for num in nums:
+            dic[num] += num
+            if num > max_num:
+                max_num = num
+        
+        dp = [0 for _ in range(max_num + 1)]
+        
+        dp[0] = 0
+        dp[1] = dic[1]
+        
+        for i in range(2, max_num + 1):
+            dp[i] = max(dp[i - 2] + dic[i], dp[i - 1])
 
-        for n in nums:
-            dic[n] += n
-
-        @cache
-        def dfs(num):
-            if num <= 0:
-                return 0 
-
-            return max(dfs(num - 2) + dic[num], dfs(num - 1))
-
-        return dfs(max(nums))
+        return dp[max_num]
+        
