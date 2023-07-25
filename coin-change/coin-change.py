bb80157b76f20f523dae0c5bbb80643a5067be48
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-25 18:58:58 -0400

    Time: 1257 ms, Memory: 46.5 MB - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,7 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
+        
+        dp = {}
+        
         def dfs(target):
+            if target in dp:
+                return dp[target]
+            
             if target < 0:
                 return -1
             
@@ -13,6 +18,7 @@ class Solution:
                 temp = dfs(target - c) 
                 if temp != -1:
                     val.append(temp)
-            return 1 + min(val) if val != [] else -1
+            dp[target] = 1 + min(val) if val != [] else -1
+            return dp[target]
         
         return dfs(amount)
