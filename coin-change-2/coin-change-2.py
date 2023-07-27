48f9d69950a01ae2442baf78aff5fd234bc04db2
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-26 20:25:08 -0400

    Time: 479 ms, Memory: 16.6 MB - LeetHub

--- coin-change-2/coin-change-2.py
+++ coin-change-2/coin-change-2.py
@@ -1,18 +1,23 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        dp = [[0 for i in range(amount + 1)] for _ in range(len(coins))]
-        
+        prev_row = [0 for i in range(amount + 1)]
+
         for row in range(len(coins) - 1, -1, -1):
+            curr_row = [0 for i in range(amount + 1)]
+            
             for col in range(amount + 1):
                 if col == 0:
-                    dp[row][col] = 1
+                    curr_row[col] = 1
                 else:    
                     if col - coins[row] >= 0:
-                        dp[row][col] += dp[row][col - coins[row]]
+                        curr_row[col] += curr_row[col - coins[row]]
 
                     if row + 1 < len(coins):
-                        dp[row][col] += dp[row + 1][col]
-                        
-        return dp[0][amount]
+                        curr_row[col] += prev_row[col]
+                    
+            prev_row = curr_row
+            
+                
+        return prev_row[amount]
                     
