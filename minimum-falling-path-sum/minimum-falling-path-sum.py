7fa17fd13b29da6ddaebafd5222bf7f630d49993
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-29 15:58:01 -0400

    Time: 124 ms, Memory: 17.2 MB - LeetHub

--- minimum-falling-path-sum/minimum-falling-path-sum.py
+++ minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -3,20 +3,17 @@ class Solution:
         
         n = len(mat)
         m = len(mat[0])
-        @cache
-        def dfs(i, j):
-            if i == n - 1:
-                return mat[i][j]
-            
-            score = float('inf')
-            if j - 1 >= 0:
-                score = min(score, dfs(i + 1, j - 1))
-            if j + 1 < m:
-                score = min(score, dfs(i + 1, j + 1))
-            score = min(score, dfs(i + 1, j))
-            return mat[i][j] + score
-        
-        min_path = float('inf')
-        for x in range(m):
-            min_path = min(min_path, dfs(0, x))
-        return min_path 
+
+        Brow = mat[n - 1]
+        for i in range(n - 2, -1, -1):
+            Arow = [0 for i in range(m - 1, -1, -1)]
+            for j in range(m):
+                if j == 0:
+                    Arow[j] = min(Brow[j], Brow[j + 1])
+                elif j == m - 1:
+                    Arow[j] = min(Brow[j], Brow[j - 1])
+                else:
+                    Arow[j] = min(Brow[j - 1], Brow[j], Brow[j + 1])
+                Arow[j] += mat[i][j]
+            Brow = Arow 
+        return min(Brow)
