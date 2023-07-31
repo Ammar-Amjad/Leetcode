d0a2fe22a30bbf869c7e792cf0763e2b77bd0e8f
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-31 00:00:34 -0400

    Time: 262 ms, Memory: 24 MB - LeetHub

--- number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         @cache
-        def dfs(i, j, t):
+        def dfs(i, t):
             if i == 0 and t == 0:
                 return 1
             elif i == 0 or t == 0:
@@ -9,11 +9,11 @@ class Solution:
             
             res = 0
             for j in range(1, k + 1):
-                res += dfs(i - 1, j, t - j) 
+                res += dfs(i - 1, t - j) 
             return res
         
         res = 0
         for j in range(1, k + 1):
-            res += dfs(n - 1, j, target - j)
+            res += dfs(n - 1, target - j)
         
         return res % (10**9 + 7)
