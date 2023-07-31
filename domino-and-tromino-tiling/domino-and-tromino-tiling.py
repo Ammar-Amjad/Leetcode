69fe2b7ad8439de59a075220b880c351d97287f6
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-31 12:48:18 -0400

    Time: 40 ms, Memory: 16.3 MB - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -4,14 +4,15 @@ class Solution:
         dp = [0 for _ in range(n + 1)]
         if n == 1:
             return 1
-        dp[1] = 1
+        back3 = 1
         if n == 2:
             return 2
-        dp[2] = 2
+        back2 = 2
         if n == 3:
             return 5
-        dp[3] = 5
+        back1 = 5
+        
         for i in range(4, n + 1):
-            dp[i] = (dp[i - 3] + (dp[i - 1] * 2))
-        return (dp[n]) % (10**9 + 7)
+            back3, back2, back1 =  back2, back1, (back3 + (back1 * 2))
+        return back1 % (10**9 + 7)
         
