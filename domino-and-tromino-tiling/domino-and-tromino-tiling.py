76826e034edf8ef5238602ce812364853baa4bc2
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-07-31 12:45:42 -0400

    Time: 38 ms, Memory: 16.4 MB - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,12 +1,17 @@
 class Solution:
-    @cache
     def numTilings(self, n: int) -> int:
+        print(n)
+        dp = [0 for _ in range(n + 1)]
         if n == 1:
             return 1
+        dp[1] = 1
         if n == 2:
             return 2
+        dp[2] = 2
         if n == 3:
             return 5
-
-        return ((self.numTilings(n - 1) * 2) + self.numTilings(n - 3)) % (10**9 + 7)
+        dp[3] = 5
+        for i in range(4, n + 1):
+            dp[i] = (dp[i - 3] + (dp[i - 1] * 2))
+        return (dp[n]) % (10**9 + 7)
         
