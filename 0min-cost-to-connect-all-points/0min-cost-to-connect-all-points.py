73c12f2a5c416630a1a999783d2dfb8850e554bd
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-08-08 01:45:50 -0400

    Time: 844 ms, Memory: 80.9 MB - LeetHub

--- 0min-cost-to-connect-all-points/0min-cost-to-connect-all-points.py
+++ 0min-cost-to-connect-all-points/0min-cost-to-connect-all-points.py
@@ -1,55 +1,41 @@
-class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1 for i in range(size)]
-        
-    def find(self, X):
-        if X == self.root[X]:
-            return X
-        self.root[X] = self.find(self.root[X])
-        return self.root[X]
-    
-    def union(self, X, Y):
-        rootX = self.find(X)
-        rootY = self.find(Y)
-        if rootX != rootY:
-            if self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-                
-            elif self.rank[rootY] < self.rank[rootX]:
-                self.root[rootY] = rootX
-                
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
+import heapq
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        costs = []
-        for i in range(len(points)):
-            x1 = points[i][0]
-            y1 = points[i][1]
-            for j in range(i + 1, len(points)):
-                x2 = points[j][0]
-                y2 = points[j][1]
-                costs.append((abs(x1 - x2) + abs(y1 - y2), i, j))
+        n = len(points)
+
+        heap = [(0, 0)]
+
+        visited = [False for i in range(n)]
+
+        cost = 0
+        edges = 0
+
+        while edges < n:
+            weight, curr = heapq.heappop(heap)
+
+            if visited[curr]:
+                continue
+            
+            visited[curr] = True
+            cost += weight
+            edges += 1
+
+            for node in range(n):
+                if not visited[node]:
+                    nCost = abs(points[curr][0] - points[node][0]) + abs(points[curr][1] - points[node][1])
+                    heapq.heappush(heap, (nCost, node))
+
+        return cost
         
         
-        costs.sort(key = lambda x: x[0])
-         
         
-        UF = UnionFind(len(points))
         
-        total = 0
-        count = 0
-        for cost, x, y in costs:
-            if count == len(points) - 1:
-                break
-                
-            if UF.find(x) != UF.find(y): 
-                count += 1
-                total += cost
-                UF.union(x, y)
         
-        return total
+        
+        
+        
+        
+        
+        
         
