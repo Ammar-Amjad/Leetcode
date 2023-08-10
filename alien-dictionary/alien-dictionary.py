e84f490eb7cfe33f6688341fd0a0ba59ee2db0ce
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-08-10 01:13:50 -0400

    Time: 41 ms, Memory: 16.3 MB - LeetHub

--- alien-dictionary/alien-dictionary.py
+++ alien-dictionary/alien-dictionary.py
@@ -1,36 +1,40 @@
 from collections import defaultdict, deque
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        graph = defaultdict(list)
+        
+        adj_list = defaultdict(list)
         in_degree = {c: 0 for word in words for c in word}
- 
+
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
+            
             for c1, c2 in zip(w1, w2):
                 if c1 != c2:
-                    if c2 not in graph[c1]:
-                        graph[c1].append(c2)
+                    if c2 not in adj_list[c1]:
+                        adj_list[c1].append(c2)
                         in_degree[c2] += 1
                     break
             else:
                 if len(w1) > len(w2):
-                    return ""
-    
+                    return ''
+                
         queue = deque()
-        
-        for c, degree in in_degree.items():
-            if degree == 0:
+
+        for c in in_degree:
+            if in_degree[c] == 0:
                 queue.append(c)
 
         res = []
-        
         while queue:
             node = queue.popleft()
             res.append(node)
-            
-            for nei in graph[node]:
+
+            for nei in adj_list[node]:
                 in_degree[nei] -= 1
                 if in_degree[nei] == 0:
                     queue.append(nei)
-        
-        return ''.join(res) if len(res) == len(in_degree) else ""
+
+        return ''.join(res) if len(res) == len(in_degree) else ''
+                
+                
+                
