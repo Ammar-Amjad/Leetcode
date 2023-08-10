e03d9a1f4eed2c7ae64ce4fe9e701917fb6a3fcd
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2023-08-10 01:02:54 -0400

    Time: 43 ms, Memory: 16.3 MB - LeetHub

--- alien-dictionary/alien-dictionary.py
+++ alien-dictionary/alien-dictionary.py
@@ -1,36 +1,36 @@
 from collections import defaultdict, deque
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        
+        graph = defaultdict(list)
+        in_degree = {c: 0 for word in words for c in word}
  
-        graph = defaultdict(set)
-        in_degree = {char: 0 for word in words for char in word}
-
-        # Step 2: Build graph and in-degree map
         for i in range(len(words) - 1):
-            word1, word2 = words[i], words[i + 1]
-            for c1, c2 in zip(word1, word2):
+            w1, w2 = words[i], words[i + 1]
+            for c1, c2 in zip(w1, w2):
                 if c1 != c2:
                     if c2 not in graph[c1]:
-                        graph[c1].add(c2)
+                        graph[c1].append(c2)
                         in_degree[c2] += 1
                     break
             else:
-                if len(word1) > len(word2):
+                if len(w1) > len(w2):
                     return ""
+    
+        queue = deque()
+        
+        for c, degree in in_degree.items():
+            if degree == 0:
+                queue.append(c)
 
-        # Step 3: Kahn's algorithm for Topological Sort
-        queue = deque([char for char, degree in in_degree.items() if degree == 0])
-        result = []
+        res = []
+        
         while queue:
-            char = queue.popleft()
-            result.append(char)
-            for neighbor in graph[char]:
-                in_degree[neighbor] -= 1
-                if in_degree[neighbor] == 0:
-                    queue.append(neighbor)
-
-        # Step 4: Check for Validity
-        if len(result) != len(in_degree):
-            return ""
-        return ''.join(result)
+            node = queue.popleft()
+            res.append(node)
+            
+            for nei in graph[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    queue.append(nei)
+        
+        return ''.join(res) if len(res) == len(in_degree) else ""
